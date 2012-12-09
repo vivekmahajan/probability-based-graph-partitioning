98d08b9d54f15388cadd759d7cc698a456013160
Vivek Mahajan <vivek2286@gmail.com>
2012-12-08 20:06:07 -0800

    commiting generating the adjancey list script

--- facebook_adjacency_list.py
+++ facebook_adjacency_list.py
@@ -2,7 +2,7 @@ import sys
 
 user_access = {}
 
-for wall_post in open("facebook-wall.txt.anon", "r"):
+for wall_post in open("./datasets/facebook-wall.txt.anon", "r"):
     users = wall_post.split("\t")
     if user_access.has_key((int(users[0]), int(users[1]))):
         user_access[(int(users[0]), int(users[1]))] += 1
@@ -17,8 +17,7 @@ for key in user_access.keys():
     #print key," ", user_access[key]
 
 adjacency_list = {}
-edges = 0
-for friends in open("facebook-links.txt.anon", "r"):
+for friends in open("./datasets/facebook-links.txt.anon", "r"):
     users = friends.split("\t")
     user1 = int(users[0])
     user2 = int(users[1])
@@ -30,15 +29,12 @@ for friends in open("facebook-links.txt.anon", "r"):
     if user_access.has_key((user1, user2)):
         adjacency_list[user1][str(user2)] = user_access[(user1, user2)] 
         adjacency_list[user2][str(user1)] = user_access[(user1, user2)]   
-        edges = edges + 1
     elif user_access.has_key((user2, user1)):
         adjacency_list[user1][str(user2)] = user_access[(user2, user1)] 
         adjacency_list[user2][str(user1)] = user_access[(user2, user1)]   
-        edges = edges + 1
     else:
         adjacency_list[user1][str(user2)] = 0 
         adjacency_list[user2][str(user1)] = 0   
-        edges = edges + 1
 
 keys = []
 for key in (adjacency_list.keys()):
@@ -46,11 +42,16 @@ for key in (adjacency_list.keys()):
     #print "**",key,"**", " ".join(adjacency_list[key])
 keys.sort()
 edges = 0
+
+for key in keys :
+    edges = edges + len(adjacency_list[key])
+
+print len(adjacency_list), edges/2
+
 for key in keys :
     for edge in adjacency_list[key].keys():
         sys.stdout.write(str(edge)+","+str(adjacency_list[key][edge])+" ")
     sys.stdout.write('\n')
-    edges = edges + len(adjacency_list[key])
 
 print >> sys.stderr, len(adjacency_list),edges/2
 
