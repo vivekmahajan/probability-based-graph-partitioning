912360f5ae654455c5dc316469287184a97d0e9e
Vivek Mahajan <vivek2286@gmail.com>
2012-12-09 22:55:31 -0800

    done graphchk and calculating weights

--- graph_partition/partition.cpp
+++ graph_partition/partition.cpp
@@ -4,5 +4,5 @@
 int main(int argc, char* argv[]){
 
    graph *g_obj = new graph(argv[1]);
-
+   g_obj->calculate_weights();
 }
--- graph_partition/partition.h
+++ graph_partition/partition.h
@@ -5,6 +5,8 @@
 #include<sstream>
 #include<boost/tokenizer.hpp>
 #include<cassert>
+#include<algorithm>
+
 using namespace std;
 
 //data strcuture for an edge
@@ -13,33 +15,59 @@ struct edge{
     int weight;
 };
 
+bool compare_function(pair<int, float> p1, pair<int, float> p2){
+    return(p1.second>p2.second);
+}
+
 class graph{
     vector< vector<struct edge> > grph;
+    vector< pair<int, float> > node_weights;
     int no_nodes, no_edges;
     public:
         graph(char *filename);
+        void calculate_weights();
         void graph_check();
 };
+void graph::calculate_weights(){
+
+    for(int i=0; i<this->grph.size(); i++){
+        float node_weight = 0.0;
+        for(int j=0; j<this->grph[i].size(); j++){
+            struct edge e = this->grph[i][j];
+            node_weight = node_weight + e.weight;
+        }
+        assert(this->grph[i].size()!=0);
+        node_weight = node_weight/this->grph[i].size();
+        node_weights.push_back(make_pair(i, node_weight));
+        assert(node_weights.size()==i+1);
+    }
+    //sorting the nodes by weight
+    sort(node_weights.begin(), node_weights.end(), compare_function);
+    for(int i=0; i<node_weights.size(); i++){
+        //cout<<node_weights[i].second<<endl;
+
+    }
+
+}
 
 void graph::graph_check(){
-    cout<<grph.size()<<"=="<<this->no_nodes<<endl;
     assert(grph.size()==this->no_nodes);
     int size=0;
     for(int i=0; i<grph.size(); i++){
         size = size+grph[i].size();
     }
-    cout<<size<<" == "<<this->no_edges<<endl;
-    //assert(size/2 == no_edges);
+    assert(size/2 == no_edges);
 }
 
 graph::graph(char* filename){
     ifstream myfile(filename);
+    string str;
     myfile >> this->no_nodes >> this->no_edges;
+    getline(myfile, str);//understand this and fix it
     vector<struct edge> temp;
     for(int i=0; i<this->no_nodes;i ++){
         grph.push_back(temp);
     }
-    string str;
     int node_no = 0;
     while(getline(myfile, str)){
         istringstream edges(str);
